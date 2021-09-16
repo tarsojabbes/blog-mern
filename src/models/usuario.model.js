a1eeb571eb2dba3af2bacb1d5f97380459ff55b5
Tarso Jabbes <tarsojabbes@gmail.com>
2021-09-16 08:42:52 -0300

    Encriptação da senha do usuário

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -10,5 +10,13 @@ const DataSchema = new mongoose.Schema({
     timestamps: true
 })
 
+DataSchema.pre('save', async function (next) {
+    if (!this.isModified("senha")) {
+        return next()
+    }
+    this.senha = await bcrypt.hashSync(this.senha_usuario, 10)
+    next()
+})
+
 const usuarios = mongoose.Model("Usuarios", DataSchema)
 module.exports = usuarios
