bcf35b3a2880158da7f0129bbdeab19a5d5f63bb
Tarso Jabbes <tarsojabbes@gmail.com>
2021-09-17 09:15:44 -0300

    CRUD de Posts

---
+++ src/controllers/post.controllers.js
@@ -0,0 +1,33 @@
+const Post = require('../models/post.model')
+const { update } = require('./usuario.controller')
+
+module.exports = {
+    async index(req, res) {
+        const posts = await Post.find()
+        res.json(posts)
+    },
+    async create(req, res) {
+        const { titulo, conteudo } = req.body
+
+        let data = {}
+        let post = await Post.findOne({ titulo })
+        if (!post) {
+            data = { titulo, conteudo }
+            post = await Post.create(data);
+            return res.status(200).json(post)
+        } else {
+            return res.status(500).json(post)
+        }
+    },
+    async delete(req, res) {
+        const { _id } = req.params
+        const post = await Post.findByIdAndDelete({ _id })
+        return res.json({ "Post removido": post })
+    },
+    async update(req, res) {
+        const { _id, titulo, conteudo } = req.body
+        const data = { _id, titulo, conteudo }
+        const post = await Post.findOneAndUpdate({ _id }, data, { new: true })
+        return res.json({ "Post atualizado": post })
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const routes = express.Router()
 const Usuario = require('./controllers/usuario.controller')
+const Post = require('./controllers/post.controllers')
 
 routes.get('/', (req, res) => {
     res.send('Página Inicial')
 })
 
+// CRUD Usuários
 routes.get('/api/usuarios', Usuario.index)
 routes.post('/api/usuarios', Usuario.create)
 routes.delete('/api/usuarios/:_id', Usuario.delete)
 routes.put('/api/usuarios', Usuario.update)
+
+// Crud Posts
+routes.get('/api/posts', Post.index)
+routes.post('/api/posts', Post.create)
+routes.delete('/api/posts/:_id', Post.delete)
+routes.put('/api/posts', Post.update)
 
 module.exports = routes
