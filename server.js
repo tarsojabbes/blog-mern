609cff47d7cc27e0a4970b747d3c32033763230e
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-02 13:28:00 -0300

    make it better

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const app = express()
 const routes = require('./src/routes')
+const { PORT = 5000, LOCAL_ADDRESS = '0.0.0.0' } = process.env
 
 mongoose.connect('mongodb+srv://root:050386999@cluster0.cqr2w.mongodb.net/blog-mern?retryWrites=true&w=majority', (err) => {
     if (err) {
@@ -18,6 +19,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(routes)
 
-app.listen(5000, () => {
-    console.log('>> Servidor está rodando na porta 5000')
+app.listen(PORT, LOCAL_ADRESS, () => {
+    const address = app.address()
+    console.log('>> Servidor está rodando em', address)
 })
