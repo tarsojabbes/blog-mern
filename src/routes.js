a266df793bb9355a829151c8e440ddb9e24f99b5
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-02 14:02:19 -0300

    make it better

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ const routes = express.Router()
 const Usuario = require('./controllers/usuario.controller')
 const Post = require('./controllers/post.controllers')
 
-routes.get('/', (req, res) => {
-    res.sendFile('C:/Users/Usuario/Desktop/blog-mern/client/public/index.html')
-})
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    routes.use(express.static('client/build'));
+
+    routes.get('*', (request, response) => {
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // CRUD Usuários
 routes.get('/api/usuarios', Usuario.index)
