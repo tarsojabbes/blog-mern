c28b8e79383a59c00101d76d91a9e0a428dccbdc
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-03 13:10:59 -0300

    make it better

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
-
+import { PORT } from '../../../server'
 import axios from 'axios'
 
-const api = axios.create({ baseURL: "http://localhost:5000" })
+const api = axios.create({ baseURL: `http://localhost:${PORT}` })
 
 export default api
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const routes = require('./src/routes')
 const { LOCAL_ADDRESS = '0.0.0.0' } = process.env
 
-const PORT = process.env.PORT || 5000
+export const PORT = process.env.PORT || 5000
 
 mongoose.connect('mongodb+srv://root:050386999@cluster0.cqr2w.mongodb.net/blog-mern?retryWrites=true&w=majority', (err) => {
     if (err) {
