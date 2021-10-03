fa831cf4cad73020266c6006303b14326c66d839
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-03 14:07:35 -0300

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const routes = require('./src/routes')
 const { LOCAL_ADDRESS = '0.0.0.0' } = process.env
 const PORT = process.env.PORT || 5000
 
-mongoose.connect('mongodb+srv://root:050386999@cluster0.cqr2w.mongodb.net/blog-mern?retryWrites=true&w=majority', (err) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:050386999@cluster0.cqr2w.mongodb.net/blog-mern?retryWrites=true&w=majority', (err) => {
     if (err) {
         console.log(err)
     }
