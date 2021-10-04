28a41a531bbc1a4df06c8edf38c14211d1a7150e
Tarso Jabbes <tarsojabbes@gmail.com>
2021-10-04 13:10:25 -0300

    Instalação do React Router Dom

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const PORT = process.env.PORT || 5000
-const api = axios.create({ baseURL: `http://localhost:${PORT}` })
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL })
 
 export default api
